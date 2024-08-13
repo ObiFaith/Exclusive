cd8544fdbbe7bb2666e65e3ef0635addd181ca73
ObiFaith <obifth@gmail.com>
2024-08-13 05:56:03 +0100

    feat/add-embla-carousel

--- src/components/SectionHeading.jsx
+++ src/components/SectionHeading.jsx
@@ -1,19 +1,49 @@
-import { arrow_left, arrow_right } from '../assets';
 import { Btn } from './Buttons';
+import { useCallback, useEffect } from 'react';
+import { arrow_left, arrow_right } from '../assets';
+import useEmblaCarousel from 'embla-carousel-react';
 
-const SectionHeading = ({ heading, btnName = '', carousel = false, to='' }) => {
+const SectionHeading = ({
+	heading,
+	btnName = '',
+	carousel = false,
+	to = '',
+}) => {
+	const [emblaRef, emblaApi] = useEmblaCarousel();
+
+	const scrollPrev = useCallback(() => {
+		if (emblaApi) emblaApi.scrollPrev();
+	}, [emblaApi]);
+
+	const scrollNext = useCallback(() => {
+		if (emblaApi) emblaApi.scrollNext();
+	}, [emblaApi]);
+
+	// Prevent the carousel from initializing when there's no need for it
+	useEffect(() => {
+		if (!carousel) {
+			emblaApi?.destroy();
+		}
+	}, [carousel, emblaApi]);
+
 	return (
 		<div className="flex justify-between items-center pb-8">
 			<h1 className="text-xl md:text-2xl lg:text-3xl">{heading}</h1>
 			{btnName && <Btn to={to} name={btnName} />}
 			{carousel && (
-				<div className="flex gap-2">
-					<div className="bg-secondary p-2.5 lg:p-4 rounded-full">
+				<div ref={emblaRef} className="flex gap-2 cursor-pointer">
+					<button
+						onClick={scrollPrev}
+						className="embla__prev bg-secondary p-2.5 lg:p-4 rounded-full"
+					>
 						<img src={arrow_left} alt="arrow_left" />
-					</div>
-					<div className="bg-secondary p-2.5 lg:p-4 rounded-full">
+					</button>
+					<button
+						onClick={scrollNext}
+						className="embla__next bg-secondary p-2.5 lg:p-4 rounded-full"
+					>
 						<img src={arrow_right} alt="arrow_right" />
-					</div>
+					</button>
 				</div>
 			)}
 		</div>
--- src/sections/FlashSales.jsx
+++ src/sections/FlashSales.jsx
@@ -1,21 +1,32 @@
-import { products } from "../constant"
-import { Btn, Card, Section, SectionHeading, SectionTitle } from ".."
+import { products } from '../constant';
+import Autoplay from 'embla-carousel-autoplay';
+import useEmblaCarousel from 'embla-carousel-react';
+import { Btn, Card, Section, SectionHeading, SectionTitle } from '..';
 
 const FlashSales = () => {
-  const salesProds = products.filter(product => product.section === 'flashSales')
+	const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+	const salesProds = products.filter(
+		product => product.section === 'flashSales'
+	);
 
-  return (
-    <Section className='overflow-hidden pr-0 mr-0'>
-      <div className="container pl-0 ml-0">
-        <SectionTitle title='Today’s' />
-        <SectionHeading carousel={true} heading='Flash Sales' />
-      </div>
-      <div className="flex gap-7 *:min-w-[270px]">
-        {salesProds.map((product, index) => <Card key={index} {...product} />)}
-      </div>
-      <div className="text-center pt-[60px] pb-5"><Btn to='/products' name='View All Products' /></div>
-    </Section>
-  )
-}
+	return (
+		<Section className="embla pr-0 mr-0">
+			<div className="container pl-0 ml-0">
+				<SectionTitle title="Today's" />
+				<SectionHeading carousel={true} heading="Flash Sales" />
+			</div>
+			<div ref={emblaRef} className="embla__viewport">
+				<div className="embla__container gap-7 *:embla__slide *:min-w-[270px]">
+					{salesProds.map((product, index) => (
+						<Card key={index} {...product} />
+					))}
+				</div>
+			</div>
+			<div className="text-center pt-[60px] pb-5">
+				<Btn to="/products" name="View All Products" />
+			</div>
+		</Section>
+	);
+};
 
-export default FlashSales
+export default FlashSales;
